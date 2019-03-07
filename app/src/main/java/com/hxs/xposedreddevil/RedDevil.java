eb8e1bea8042584847fc40b5c973156c1c2f14d0
蹲在旮旯造卷饼 <1013455890@qq.com>
2019-03-07 17:06:41 +0800

    1.修复xposed微信7.0.3无法抢红包的问题；

--- app/src/main/java/com/hxs/xposedreddevil/RedDevil.java
+++ app/src/main/java/com/hxs/xposedreddevil/RedDevil.java
@@ -1,8 +1,10 @@
 package com.hxs.xposedreddevil;
 
 import com.hxs.xposedreddevil.contentprovider.PropertiesUtils;
+import com.hxs.xposedreddevil.hook.LivePalyerHook;
 import com.hxs.xposedreddevil.hook.RedHook;
 import com.hxs.xposedreddevil.hook.RedHook673;
+import com.hxs.xposedreddevil.hook.RedHook703;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -14,9 +16,12 @@ public class RedDevil implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+//        LivePalyerHook.getInstance().init(lpparam);
         log("微信版本:" + PropertiesUtils.getValue(RED_FILE, "wechatversion", ""));
         if (PropertiesUtils.getValue(RED_FILE, "wechatversion", "").equals("")) {
             RedHook673.getInstance().init(lpparam);
+        } else if (PropertiesUtils.getValue(RED_FILE, "wechatversion", "").equals("7.0.3")) {
+            RedHook703.getInstance().init(lpparam);
         } else if (PropertiesUtils.getValue(RED_FILE, "wechatversion", "").equals("7.0.0")) {
             RedHook.getInstance().init(lpparam);
         } else if (PropertiesUtils.getValue(RED_FILE, "wechatversion", "").equals("6.7.3")) {
