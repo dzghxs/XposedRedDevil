dda2cd4450f044356f8dad98f65accd282126408
韩雪松 <1013455890@qq.com>
2023-09-18 16:19:53 +0800

    适配8.0.42

--- app/src/main/java/com/hxs/xposedreddevil/utils/AcxiliaryServiceStaticValues.java
+++ app/src/main/java/com/hxs/xposedreddevil/utils/AcxiliaryServiceStaticValues.java
@@ -82,9 +82,9 @@ public class AcxiliaryServiceStaticValues {
 
         LuckyMoneyNotHookReceiveUI = "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyNotHookReceiveUI";
         LuckyMoneyNotHookReceiveUIMethod = "onSceneEnd";
-        LuckyMoneyNotHookReceiveUIMethodParameter = "hd0.y";      //全局搜索getIsKinda
-        LuckyMoneyNotHookReceiveUIButton = "o";                               //LuckyMoneyNotHookReceiveUI中的button
-        handleLuckyMoney = "lp3.b";                               //全局搜索add(".ui.transmit.SelectConversationUI") Log.i("MicroMsg.PluginHelper", "start multi webview!!!!!!!!!")
+        LuckyMoneyNotHookReceiveUIMethodParameter = "yf0.y";      //全局搜索TAG = "MicroMsg.NetSceneBase"
+        LuckyMoneyNotHookReceiveUIButton = "p";                               //LuckyMoneyNotHookReceiveUI中的button
+        handleLuckyMoney = "sv3.b";                               //全局搜索context.getSharedPreferences(MMApplicationContext.getDefaultPreferencePath(), 0).getBoolean("settings_multi_webview", false) && ".ui.tools.WebViewUI".endsWith(str2)+
         handleLuckyMoneyMethod = "i";
         handleLuckyMoneyClass = ".ui.LuckyMoneyNotHookReceiveUI";
 //        }
