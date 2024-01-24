d8e5cd2074f4327f42f0a766460bdec081ccd222
韩雪松 <hanxs@dehuinet.com>
2024-01-24 14:40:47 +0800

    适配8.0.46

--- app/src/main/java/com/hxs/xposedreddevil/utils/AcxiliaryServiceStaticValues.java
+++ app/src/main/java/com/hxs/xposedreddevil/utils/AcxiliaryServiceStaticValues.java
@@ -80,7 +80,7 @@ public class AcxiliaryServiceStaticValues {
         LuckyMoneyNotHookReceiveUIMethod = "onSceneEnd";
         LuckyMoneyNotHookReceiveUIMethodParameter = "com.tencent.mm.modelbase.k1";      //全局搜索TAG = "MicroMsg.NetSceneBase"
         LuckyMoneyNotHookReceiveUIButton = "p";                               //LuckyMoneyNotHookReceiveUI中的button
-        handleLuckyMoney = "v14.l";                               //全局搜索add(".ui.transmit.SelectConversationUI");
+        handleLuckyMoney = "w24.l";                               //全局搜索add(".ui.transmit.SelectConversationUI");
         handleLuckyMoneyMethod = "i";
         handleLuckyMoneyClass = ".ui.LuckyMoneyNotHookReceiveUI";
 //        }
